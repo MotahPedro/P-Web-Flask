48fa6053354e7201e6ce6a2a0778f4f88054c949
MotahPedro <motahpedro@gmail.com>
2024-09-10 21:52:15 -0300

    first commit

---
+++ Exercicio/exercicio/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template ('index.html')
+
+@app.route('/about')
+def about():
+    return render_template ('about.html')
+
+@app.route('/menu')
+def menu():
+    return render_template ('menu.html')
+
+@app.route('/reservations')
+def reservations():
+    return render_template ('reservation.html')
+
+@app.route('/special-dishes')
+def special():
+    return render_template ('special-dishes.html')
+
+@app.route('/team')
+def team():
+    return render_template ('team.html')
+
+app.run(debug=True)
