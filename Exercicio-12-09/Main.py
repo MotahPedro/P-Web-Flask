729bcffa813e65d218785375ac39cda53eb37343
MotahPedro <motahpedro@gmail.com>
2024-09-12 22:19:29 -0300

    update: aula 12/09 completa

--- Exercicio-12-09/Main.py
+++ Exercicio-12-09/Main.py
@@ -4,4 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template ('index.html')
+    return render_template ('imc.html')
+
+@app.route('/calcular_imc_post', methods=['POST']) # Rota para o método POST do formulário corresponde a action="/calcular_imc_post"
+def calcular_imc():
+    return 'deu certo'
+
+
+app.run(debug=True)
