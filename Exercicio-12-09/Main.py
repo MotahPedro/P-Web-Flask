32a55190173b63e3fe2bc88d686bc9ee523bf670
MotahPedro <motahpedro@gmail.com>
2024-09-17 21:43:32 -0300

    feat: lógica do IMC implementada

--- Exercicio-12-09/Main.py
+++ Exercicio-12-09/Main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
+
+# Flask é a base para a aplicação
+# render_template habilida a  renderização pelas rotas
+# request puxa o dado da rota de acordo com o nome do input do form
 
 app = Flask(__name__)
 
@@ -8,7 +12,10 @@ def index():
 
 @app.route('/calcular_imc_post', methods=['POST']) # Rota para o método POST do formulário corresponde a action="/calcular_imc_post"
 def calcular_imc():
-    return 'deu certo'
-
+    altura = float(request.form['txt_altura'])
+    peso = float(request.form['txt_peso'])
+    imc = peso/(altura*altura)
+    # Usar uma f-string, você pode incluir expressões Python dentro de chaves {} dentro da string. Essas expressões serão avaliadas e seus resultados serão incorporados na string final.
+    return f'Seu IMC é: {imc:.2f}'
 
 app.run(debug=True)
